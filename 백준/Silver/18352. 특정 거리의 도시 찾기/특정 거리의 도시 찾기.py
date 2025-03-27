1bae5ef645138c707a236c7b09f9bfc7a3c60f02
Woojung Byeon <ricenuu62@gmail.com>
2025-03-27 09:25:01 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1396 ms, Memory: 100836 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,6 +1,8 @@
 # 백준 18352
 # 탐색, 최단거리 -> BFS
 # 방문 순서 중요하지 않은데, 거리가 중요함
+# (기억) 다음 연결점이 거리 q 넘으면 다음 연결점들이 k와 비교하여 append하는 것을 멈춤
+
 from collections import deque
 import sys
 
@@ -14,21 +16,20 @@ def bfs():
             if distance_list[q] == -1:
                 queue.append(q)
                 distance_list[q] = distance_list[p] + 1  # 중요!! 현재 거리 + 1을 넣어줌
+
+                # (중요!!) 다음 연결된 점이 최단 거리가 k를 넘을 때(inf는 아니고, k를 넘어갈 때) result 추가하는 것을 중단함
+                # 다음 연결된 점이 k 넘으면 다른 연결점도 k 넘을 것이고, 계속 넘을 것임
+                if distance_list[q] == k+1: 
+                    return
                 
                 # 거리가 k인 점을 result에 추가
                 if distance_list[q] == k:
                     result.append(q)
-    if not result:
-        print (-1)
-
-    else:
-        result.sort()
-        for i in range(len(result)):
-            print (result[i])
 
 
-input = sys.stdin.readline
-n, m, k, x = map(int, input().split())
+
+input_sys = sys.stdin.readline
+n, m, k, x = map(int, input_sys().split())
 graph = [[] for _ in range(n+1)] # 0번은 빈 리스트
 
 # distance_list : x에서 해당 노드에 최단거리
@@ -41,7 +42,16 @@ result = []
 
 # graph 생성
 for _ in range(m):
-    a, b = map(int, input().split())
+    a, b = map(int, input_sys().split())
     graph[a].append(b)
 
 bfs()
+
+
+if not result:
+    print (-1)
+            
+else:
+    result.sort()
+    for answer in result:
+        print (answer)
