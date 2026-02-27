68de140f6ccdb2df67d380cb1770c4bc63c19459
Woojung Byeon <ricenuu62@gmail.com>
2026-02-27 17:22:25 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,21 +1,22 @@
+def convert(target):
+    return min(ord(target)-ord('A'), ord('Z')-ord(target)+1)
+
+
 def solution(name):
-    answer = 0
-    n = len(name)
-
-    def alphabet_to_num(char):
-        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
-        return num_char[ord(char) - ord('A')]
-
-    for ch in name:
-        answer += alphabet_to_num(ch)
-
-    move = n - 1
-    for idx in range(n):
-        next_idx = idx + 1
-        while (next_idx < n) and (name[next_idx] == 'A'):
-            next_idx += 1
-        distance = min(idx, n - next_idx)
-        move = min(move, idx + n - next_idx + distance)
-
-    answer += move
-    return answer
+    name_list = list(name)
+    name_length = len(name_list)
+    move_count = name_length - 1
+    
+    convert_count = 0
+    for i, alp in enumerate(name_list):
+        convert_count += convert(alp)
+        
+        j = i + 1
+        # Convert 해야 할 마지막 인덱스 찾기
+        while j < name_length and name_list[j] == 'A':
+            j += 1
+        
+        # min(전체 단방향, i까지 갔다가 반대로 돌아서 j까지, 반대로 j까지 갔다가 i로 되돌아오는 것)
+        move_count = min(move_count, i * 2 + (name_length-j), (name_length-j)*2 + i)
+        
+    return convert_count + move_count
